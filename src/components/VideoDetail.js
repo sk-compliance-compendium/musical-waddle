1da04d016172e2fac5c2decb8e97bc510d1f4d6a
sk-compliance-compendium <swaminathan.k@akonisoftwareservices.com>
2019-04-10 14:03:16 +0530

    Getting the Video Up and Running! Seeing Thalaiva Man! Can you beleive it!

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,9 +4,18 @@ const VideoDetail = ({video}) => {
     if(!video){
         return <div>Loading...</div>;
     }
+
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
     return (
         <div>
-            {video.snippet.title}
+            <div className="ui embed">
+                <iframe src={videoSrc} frameBorder = "0" allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
+            </div>
+            <div className="ui segment">
+                <h4 className="ui header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
         </div>
     );
 };
