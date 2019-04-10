14ddb823194c0c00df2ec01795cc5c176b99a50b
sk-compliance-compendium <swaminathan.k@akonisoftwareservices.com>
2019-04-10 13:39:27 +0530

    Bringing in Video Detail Yo!

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
-class App extends React.Component{
+class App extends React.Component {
 
     state = {
-        videos: []
+        videos: [],
+        selectedVideo: null
     };
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -18,11 +20,16 @@ class App extends React.Component{
         this.setState({videos: response.data.items});
     };
 
-    render(){
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    };
+
+    render() {
         return (
             <div className="ui container">
-                <SearchBar onFormSubmit = {this.onTermSubmit} />
-                <VideoList videos= {this.state.videos}/>
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
             </div>
         );
     }
